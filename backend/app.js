3e0c99b37ebd66c6c71726d615955f3d3fb5431d
techarm <techarmdev@gmail.com>
2023-01-01 09:42:16 +0900

    Adding Place and User routes

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placeRoutes = require('./routes/places-routes');
+
+const app = express();
+
+// URI => /api/place/...
+app.use('/api/places', placeRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+app.listen(3001);
