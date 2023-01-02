0784de3809bb813bd4626b165300c8c0190b5c65
techarm <techarmdev@gmail.com>
2023-01-02 18:04:02 +0900

    Handing Errors for Unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placeRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 // URI => /api/place/...
 app.use('/api/places', placeRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError(`Counld not find ${req.url}`, 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
