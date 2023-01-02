b9ed74ccdaa8cd1a507191e36594eac627a05455
techarm <techarmdev@gmail.com>
2023-01-02 20:16:26 +0900

    Adding update place and delete place handle

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -1,7 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -43,13 +43,13 @@ const getPlaceById = (req, res, next) => {
 
 const getPlaceByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => p.creator === userId);
+  const places = DUMMY_PLACES.filter((p) => p.creator === userId);
 
-  if (!place) {
+  if (!places) {
     return new HttpError('Could not find a place for the user id.', 404);
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -67,11 +67,31 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-// exports.getPlaceById = getPlaceById;
-// exports.getPlaceByUserId = getPlaceByUserId;
-// exports.createPlace = createPlace;
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id != placeId);
+
+  res.status(200).json({ message: 'Deleted place.' });
+};
+
 module.exports = {
   getPlaceById,
   getPlaceByUserId,
   createPlace,
+  updatePlace,
+  deletePlace,
 };
