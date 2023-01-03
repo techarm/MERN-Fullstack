686053d8896855ce98125c3abd5b5c5b3e543d34
techarm <techarmdev@gmail.com>
2023-01-03 21:40:50 +0900

    Handing CORS Errors

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 // URI => /api/place/...
 app.use('/api/places', placeRoutes);
 app.use('/api/users', userRoutes);
