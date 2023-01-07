427ae24b2eda5901c794af820d721cbd71d9c71f
techarm <techarmdev@gmail.com>
2023-01-07 17:31:21 +0900

    Hashing the User Password & Generating tokens (JWT)

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
+
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
+const JWT_TOKEN_SECRET_KEY = process.env.JWT_TOKEN_SECRET_KEY;
 
 const getAllUsers = async (req, res, next) => {
   let users;
@@ -35,9 +39,18 @@ const signup = async (req, res, next) => {
       );
     }
   } catch (err) {
-    console.err(`signup - findOne error: ${err}`);
+    console.error(`signup - findOne error: ${err}`);
     return next(
       new HttpError('Signing up failed, please try again later', 500)
+    );
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    return next(
+      new HttpError('Could not create user, please try again later', 500)
     );
   }
 
@@ -45,7 +58,7 @@ const signup = async (req, res, next) => {
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
 
@@ -56,7 +69,21 @@ const signup = async (req, res, next) => {
     return next(new HttpError('Signing up failed, please try again.', 500));
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      JWT_TOKEN_SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+  } catch (err) {
+    console.error(err);
+    return next(new HttpError('Signing up failed, please try again.', 500));
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -71,7 +98,7 @@ const login = async (req, res, next) => {
 
   try {
     const identifiedUser = await User.findOne({ email: email });
-    if (!identifiedUser || identifiedUser.password !== password) {
+    if (!identifiedUser) {
       return next(
         new HttpError(
           'Could not identify user, credentials seem to be wrong.',
@@ -79,9 +106,33 @@ const login = async (req, res, next) => {
         )
       );
     }
-    res.status(200).json({ user: identifiedUser.toObject({ getters: true }) });
+
+    let isValidPassword = await bcrypt.compare(
+      password,
+      identifiedUser.password
+    );
+    if (!isValidPassword) {
+      return next(
+        new HttpError(
+          'Could not identify user, credentials seem to be wrong.',
+          401
+        )
+      );
+    }
+
+    let token = jwt.sign(
+      { userId: identifiedUser.id, email: identifiedUser.email },
+      JWT_TOKEN_SECRET_KEY,
+      { expiresIn: '1h' }
+    );
+
+    res.status(200).json({
+      userId: identifiedUser.id,
+      email: identifiedUser.email,
+      token: token,
+    });
   } catch (err) {
-    console.err(`signup - findOne error: ${err}`);
+    console.error(`signup - error: ${err}`);
     return next(
       new HttpError('Signing up failed, please try again later', 500)
     );
