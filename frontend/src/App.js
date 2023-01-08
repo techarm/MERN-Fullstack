54ac4056f230255c417e5192feff47fe0434d831
techarm <techarmdev@gmail.com>
2023-01-08 11:19:53 +0900

    Adding Auto-Logi & Auto-Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -13,19 +13,62 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState();
 
-  const loginHandler = useCallback((userId, token) => {
+  const loginHandler = useCallback((userId, token, expirationDate) => {
     setUserId(userId);
     setToken(token);
+
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
+
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: userId,
+        token: token,
+        expiration: tokenExpirationDate.toString(),
+      })
+    );
   }, []);
 
   const logoutHandler = useCallback(() => {
     setUserId(null);
     setToken(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logoutHandler, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logoutHandler, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      loginHandler(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      );
+    }
+  }, [loginHandler]);
 
   let routes;
   if (token) {
