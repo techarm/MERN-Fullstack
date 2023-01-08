f4c6fc68e491b1e2b8c6e318b591cf56a806d534
techarm <techarmdev@gmail.com>
2023-01-08 11:44:48 +0900

    Creating a Custom Authentication hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import { useCallback, useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -12,63 +11,15 @@ import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
-
-let logoutTimer;
+import useAuth from './shared/hooks/auth-hook';
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-  const [userId, setUserId] = useState();
-
-  const loginHandler = useCallback((userId, token, expirationDate) => {
-    setUserId(userId);
-    setToken(token);
-
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExpirationDate);
-
-    localStorage.setItem(
-      'userData',
-      JSON.stringify({
-        userId: userId,
-        token: token,
-        expiration: tokenExpirationDate.toString(),
-      })
-    );
-  }, []);
-
-  const logoutHandler = useCallback(() => {
-    setUserId(null);
-    setToken(null);
-    setTokenExpirationDate(null);
-    localStorage.removeItem('userData');
-  }, []);
-
-  useEffect(() => {
-    if (token) {
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logoutHandler, remainingTime);
-    } else {
-      clearTimeout(logoutTimer);
-    }
-  }, [token, logoutHandler, tokenExpirationDate]);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      loginHandler(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expiration)
-      );
-    }
-  }, [loginHandler]);
+  const {
+    userId,
+    token,
+    login: loginHandler,
+    logout: logoutHandler,
+  } = useAuth();
 
   let routes;
   if (token) {
